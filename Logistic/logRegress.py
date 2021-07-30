cd8baed3578d056cfba60341fdf751492ed68822
coderLCJ <2907805535@qq.com>
2021-07-30 22:24:14 +0800

    update

---
+++ Logistic/logRegress.py
@@ -0,0 +1,19 @@
+def loadDataSet():
+    dataMat = []
+    labelMat = []
+    fr = open('testSet.txt', 'r')
+    for line in fr.readlines():
+        lineArr = line.strip().split()
+        dataMat.append([1.0, float(lineArr[0]), float(lineArr[1])])
+        labelMat.append(int(lineArr[2]))
+    return dataMat, labelMat
+
+def sigmoid(inX):
+    return 1.0/(1+exp(-inX))
+
+def gradAscent(dataMatIn, classLabels):
+    dataMatrix = mat(dataMatIn)
+
+
+dataMat, labelMat = loadDataSet()
+print(dataMat, '\n', labelMat)
