2915957902a429980f1e72603fcc554f51dbf00f
coderLCJ <2907805535@qq.com>
2021-08-03 22:02:22 +0800

    update

--- Logistic/logRegress.py
+++ Logistic/logRegress.py
@@ -26,7 +26,38 @@ def gradAscent(dataMatIn, classLabels):
         weights = weights + alpha * dataMatrix.transpose() * error  # dataMatrix和error前后顺序不能调换
     return weights
 
+def plotBestfit(wei):
+    import matplotlib.pyplot as plt
+    weights = wei.getA()
+    dataMat, labelMat = loadDataSet()
+    dataArr = array(dataMat)
+    n = shape(dataArr)[0]
+    xcord1 = []
+    xcord2 = []
+    ycord1 = []
+    ycord2 = []
+    for i in range(n):
+        if int(labelMat[i]) == 1:
+            xcord1.append(dataArr[i, 1])
+            ycord1.append(dataArr[i, 2])
+        else:
+            xcord2.append(dataArr[i, 1])
+            ycord2.append(dataArr[i, 2])
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(xcord1, ycord1, s=30, c='red', marker='s')
+    ax.scatter(xcord2, ycord2, s=30, c='green')
+    x = arange(-3.0, 3.0, 0.1)
+    y = (-weights[0]-weights[1]*x)/weights[2]   # 令sigmoid函数的输入为0解出x2的表达式，即分界线的方程式
+    ax.plot(x, y)
+    plt.xlabel('X1')
+    plt.ylabel('X2')
+    plt.show()
+
 dataMat, labelMat = loadDataSet()
 # print(dataMat, '\n', labelMat)
 weights = gradAscent(dataMat, labelMat)
 print(weights)
+plotBestfit(weights)
+
+
