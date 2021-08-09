3ac6affcdf7a2d5999c54b7d9a3caf7b5f5070ea
coderLCJ <2907805535@qq.com>
2021-08-09 17:57:06 +0800

    update

--- Regression/regression.py
+++ Regression/regression.py
@@ -1,4 +1,5 @@
 from numpy import *
+import matplotlib.pyplot as plt
 
 def loadDataSet(fileName):
     numFeat = len(open(fileName).readline().split('\t')) - 1
@@ -12,11 +13,11 @@ def loadDataSet(fileName):
             lineArr.append(float(curLine[i]))
         dataMat.append(lineArr)
         labelMat.append(float(curLine[-1]))
-        return dataMat, labelMat
+    return dataMat, labelMat
 
 def standRegress(xArr, yArr):
     xMat = mat(xArr)
-    yMat = mat(yArr)
+    yMat = mat(yArr).T
     xTx = xMat.T * xMat
     if linalg.det(xTx) == 0.0:      # 计算行列式
         print('This matrix is singular, cannot reverse')
@@ -24,5 +25,21 @@ def standRegress(xArr, yArr):
     ws = xTx.I * (xMat.T*yMat)
     return ws
 
+def draw(xArr, yArr, ws):
+    xMat = mat(xArr)
+    yMat = mat(yArr)
+    yHat = xMat * ws
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(xMat[:, 1].flatten().A[0], yMat.T[:, 0].flatten().A[0])
+    xCopy = xMat.copy()
+    xCopy.sort(0)
+    yHat = xCopy * ws
+    ax.plot(xCopy[:, 1], yHat)
+    plt.show()
 
 
+xArr, yArr = loadDataSet('ex0.txt')
+ws = standRegress(xArr, yArr)
+# print(ws)
+draw(xArr, yArr, ws)
