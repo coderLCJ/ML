23adb78bb26122c333bb855f65ed3254ceda3060
coderLCJ <2907805535@qq.com>
2021-08-13 21:34:22 +0800

    update

--- DeepLearning/DataProcessing/read.py
+++ DeepLearning/DataProcessing/read.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import torch
 
 data = pd.read_csv('data/house_tiny.csv')
 print(data)
@@ -7,5 +7,14 @@ print(data)
 # 处理缺失值
 inputs = data.iloc[:, 0:2]
 outputs = data.iloc[:, 2]
-inputs = inputs.fillna(inputs.mean())
+
+inputs = inputs.fillna(inputs.mean())   # 用均值替换缺失值
 print(inputs)
+
+inputs = pd.get_dummies(inputs, dummy_na=True)
+print(inputs)
+
+# 转换为张量
+x = torch.tensor(inputs.values)
+y = torch.tensor(outputs.values)
+print(x, '\n', y)
