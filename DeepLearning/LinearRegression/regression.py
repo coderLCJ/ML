ade94b180268c83ec8fbb1b7ef9d1f2fbf5cf1d1
coderLCJ <2907805535@qq.com>
2021-08-17 22:13:11 +0800

    update

--- DeepLearning/LinearRegression/regression.py
+++ DeepLearning/LinearRegression/regression.py
@@ -1,17 +1,17 @@
 import torch
-import random
+from matplotlib import pyplot as plt
 
-# def synthetic_data(w, b, num_examples):
-#     X = torch.normal(0, 1, size=(num_examples, len(w)))
-#     y = torch.matmul(X, w) + b
-#     y += torch.normal(0, 0.001, size=y.shape)
-#     return X, y.reshape((-1, 1))
-#
-#
-# true_w = torch.tensor([2, -3.4])
-# true_b = 4.2
-# features, labels = synthetic_data(true_w, true_b, 10)
-# print(features, labels)
+def synthetic_data(w, b, num_examples):
+    X = torch.normal(0, 1, (num_examples, len(w)))
+    y = torch.matmul(X, w) + b
+    y += torch.normal(0, 0.001, y.shape)
+    return X, y.reshape((-1, 1))
 
-x = torch.normal(mean=0.5, std=torch.arange(1, 6))
-print(x)
+
+true_w = torch.tensor([2, -3.4])
+true_b = 4.2
+features, labels = synthetic_data(true_w, true_b, 10000)
+print(features[0], labels[0])
+
+plt.scatter(features[:, 1], labels, 1, 'red')
+plt.show()
