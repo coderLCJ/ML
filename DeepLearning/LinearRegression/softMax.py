1f9701319eff0ac298571df48eed3c1d0f7f101f
coderLCJ <2907805535@qq.com>
2021-08-20 11:16:06 +0800

    update

---
+++ DeepLearning/LinearRegression/softMax.py
@@ -0,0 +1,165 @@
+from d2l import torch as d2l
+import torch
+from matplotlib import transforms, pyplot as plt
+from torch.utils import data
+import torchvision
+from Accumulator import *
+from Animator import *
+
+
+def softmax(X):
+    X_exp = torch.exp(X)
+    partition = X_exp.sum(1, keepdim=True)
+    return X_exp / partition
+
+
+def get_fashion_mnist_labels(labels):
+    # labels是一个下标list 返回对应的标签文本
+    text_labels = ['t-shirt', 'trouser', 'pullover', 'dress', 'coat',
+                   'sandal', 'shirt', 'sneaker', 'bag', 'ankle boot']
+    return [text_labels[int(i)] for i in labels]
+
+
+def show_images(imgs, num_rows, num_cols, titles=None, scale=1.5):
+    """Plot a list of images."""
+    figsize = (num_cols * scale, num_rows * scale)
+    _, axes = plt.subplots(num_rows, num_cols, figsize=figsize)
+    axes = axes.flatten()
+    for i, (ax, img) in enumerate(zip(axes, imgs)):
+        if torch.is_tensor(img):
+            ax.imshow(img.numpy())
+        else:
+            ax.imshow(img)
+        ax.axes.get_xaxis().set_visible(False)
+        ax.axes.get_yaxis().set_visible(False)
+        if titles:
+            ax.set_title(titles[i])
+    d2l.plt.show()
+    return axes
+
+
+def get_dataloader_workers():
+    return 0  # 进程数
+
+
+def load_data_fashion_mnist(batch_size, resize=None):  # @save
+    """下载Fashion-MNIST数据集，然后将其加载到内存中。"""
+    trans = [torchvision.transforms.ToTensor()]
+    if resize:
+        trans.insert(0, torchvision.transforms.Resize(resize))
+    trans = torchvision.transforms.Compose(trans)
+    mnist_train = torchvision.datasets.FashionMNIST(
+        root="../data", train=True, transform=trans, download=False)
+    mnist_test = torchvision.datasets.FashionMNIST(
+        root="../data", train=False, transform=trans, download=False)
+    return (data.DataLoader(mnist_train, batch_size, shuffle=True,
+                            num_workers=get_dataloader_workers()),
+            data.DataLoader(mnist_test, batch_size, shuffle=False,
+                            num_workers=get_dataloader_workers()))
+    # X, y = next(iter(data.DataLoader(mnist_train, batch_size=18)))
+    # show_images(X.reshape(18, 28, 28), 2, 9, titles=get_fashion_mnist_labels(y))
+
+
+def net(X):
+    return softmax(torch.matmul(X.reshape(-1, w.shape[0]), w) + b)
+
+
+def cross_entropy(y_hat, y):
+    # 求交叉熵
+    return -torch.log(y_hat[range(len(y_hat)), y])
+
+
+def accuracy(y_hat, y):
+    if len(y_hat.shape) > 1 and y_hat.shape[1] > 1:
+        y_hat = y_hat.argmax(axis=1)
+    cmp = y_hat.type(y.dtype) == y
+    return float(cmp.type(y.dtype).sum())
+
+
+def evaluate_accuracy(net, data_iter):
+    if isinstance(net, torch.nn.Module):
+        net.eval()  # 将模型设置为评估模式
+    metric = Accumulator(2)
+    for X, y in data_iter:
+        metric.add(accuracy(net(X), y), y.numel())  # 累计算对多少个 和 总共多少个 相除得到正确率
+    return metric[0] / metric[1]
+
+
+def train_epoch_ch3(net, train_iter, loss, updater):
+    """训练模型⼀个迭代周期（定义⻅第3章）。"""
+    # 将模型设置为训练模式
+    if isinstance(net, torch.nn.Module):
+        net.train()
+    # 训练损失总和、训练准确度总和、样本数
+    metric = Accumulator(3)
+    c = 0
+    for X, y in train_iter:
+        # 计算梯度并更新参数
+        y_hat = net(X)
+        l = loss(y_hat, y)
+        if isinstance(updater, torch.optim.Optimizer):
+            # 使⽤PyTorch内置的优化器和损失函数
+            updater.zero_grad()
+            l.backward()
+            updater.step()
+            metric.add(float(l) * len(y), accuracy(y_hat, y),
+                       y.size().numel())
+        else:
+            # 使⽤定制的优化器和损失函数
+            l.sum().backward()
+            updater(X.shape[0])
+            metric.add(float(l.sum()), accuracy(y_hat, y), y.numel())
+        print('.', end='');
+        c += 1
+        if c % 20 == 0:
+            print('')
+    # 返回训练损失和训练准确率
+    return metric[0] / metric[2], metric[1] / metric[2]
+
+
+def train_ch3(net, train_iter, test_iter, loss, num_epochs, updater):  # @save
+    """训练模型（定义⻅第3章）。"""
+    animator = Animator(xlabel='epoch', xlim=[1, num_epochs], ylim=[0.3, 0.9],
+                        legend=['train loss', 'train acc', 'test acc'])
+    for epoch in range(num_epochs):
+        train_metrics = train_epoch_ch3(net, train_iter, loss, updater)
+        test_acc = evaluate_accuracy(net, test_iter)
+        print('start')
+        animator.add(epoch + 1, train_metrics + (test_acc,))
+        print('end')
+    train_loss, train_acc = train_metrics
+    assert train_loss < 0.5, train_loss
+    assert train_acc <= 1 and train_acc > 0.7, train_acc
+    assert test_acc <= 1 and test_acc > 0.7, test_acc
+
+
+def updater(batch_size):
+    return d2l.sgd([w, b], lr, batch_size)
+
+
+def predict_ch3(net, test_iter, n=6):  # @save
+    """预测标签（定义⻅第3章）。"""
+    for X, y in test_iter:
+        break
+    trues = d2l.get_fashion_mnist_labels(y)
+    preds = d2l.get_fashion_mnist_labels(net(X).argmax(axis=1))
+    titles = [true + '\n' + pred for true, pred in zip(trues, preds)]
+    show_images(X[0:n].reshape((n, 28, 28)), 1, n, titles=titles[0:n])
+
+
+batch_size = 256
+train_iter, test_iter = load_data_fashion_mnist(32, resize=28)
+
+num_inputs = 784
+num_outputs = 10
+w = torch.normal(0, 0.01, size=(num_inputs, num_outputs), requires_grad=True)
+b = torch.zeros(num_outputs, requires_grad=True)
+
+lr = 0.1
+num_epochs = 10
+train_ch3(net, train_iter, test_iter, cross_entropy, num_epochs, updater)
+
+print('w = ', w)
+print('b = ', b)
+
+predict_ch3(net, test_iter)
